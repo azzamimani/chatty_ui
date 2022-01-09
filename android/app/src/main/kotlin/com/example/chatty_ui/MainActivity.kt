ae136cbae6cf7796c3892dbca63f3606d6d4c399
azzamimani <azzamimani240@gmail.com>
2022-01-09 21:31:48 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/chatty_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatty_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/chat_tile.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:chatty_ui/theme.dart';
+
+class ChatTile extends StatelessWidget {
+  // const ChatTile({Key? key}) : super(key: key);
+
+  final String imageUrl;
+  final String name;
+  final String text;
+  final String time;
+  final bool unread;
+
+  ChatTile(
+      {required this.imageUrl,
+      required this.name,
+      required this.text,
+      required this.time,
+      required this.unread});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 16),
+        child: Row(
+          children: [
+            Image.asset(imageUrl),
+            SizedBox(
+              height: 12,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(name, style: titleTextStyle),
+                Text(text,
+                    style: unread
+                        ? subtitleTextStyle.copyWith(color: blackColor)
+                        : subtitleTextStyle),
+              ],
+            ),
+            Spacer(),
+            Text(
+              time,
+              style: subtitleTextStyle,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
